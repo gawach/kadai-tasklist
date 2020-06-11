fa965fb3e2cfd9b9c3a622aaa988f8b320ec0a6e
gawach <gawa9.1998@gmail.com>
2020-06-11 00:08:58 +0000

    Add tasks controller

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,29 @@
+class TasksController < ApplicationController
+  def index
+  end
+  
+  def show
+  end
+
+  def new
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+
+  def update
+  end
+  
+  def destroy
+  end
+  
+  
+  private
+  
+  def message_params
+      params.require(:message).permit(:content)
+  end
+end
