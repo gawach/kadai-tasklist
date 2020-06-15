979244f35c7d45864996c3815a23360b128b0309
gawach <gawa9.1998@gmail.com>
2020-06-15 10:34:44 +0000

    Add sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,30 @@
 class UsersController < ApplicationController
   def index
   end
-
+  
   def show
   end
 
   def new
+    @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user
+      flash[:sucess] = 'ユーザーの登録に成功しました'
+      redirect_to users_show_path
+    else
+      flash.now[:danger] = 'ユーザ登録に失敗しました'
+      render :new
+    end
+  end
+  
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
 end
