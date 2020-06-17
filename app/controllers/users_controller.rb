201338c62dd118a5729e0c6744ca58cbbedb5d97
gawach <gawa9.1998@gmail.com>
2020-06-17 07:21:42 +0000

    Add signup login logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
   def index
+    @user = User.order(id: :desc).page(params[:page]).per(25)
   end
   
   def show
+    @user = User.find(params[:id])
   end
 
   def new
@@ -11,9 +13,10 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
-    if @user
+    
+    if @user.save
       flash[:sucess] = 'ユーザーの登録に成功しました'
-      redirect_to users_show_path
+      redirect_to @user
     else
       flash.now[:danger] = 'ユーザ登録に失敗しました'
       render :new
@@ -23,8 +26,8 @@ class UsersController < ApplicationController
   
   private
   
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
   
 end
